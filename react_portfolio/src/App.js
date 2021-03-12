4b0d51fed14a4aa5dfacb4b02999083b6490424b
BEhnert <behnert77@gmail.com>
2021-03-12 01:59:09 -0600

    publish

--- react_portfolio/src/App.js
+++ react_portfolio/src/App.js
@@ -15,7 +15,7 @@ function App() {
         <div>
         <NavBar />  
         <Wrapper>
-          <Route exact path="/*" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/resume" component={Resume} />
         </Wrapper>
